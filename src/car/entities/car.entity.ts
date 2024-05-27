f04e1a84089db8df1f01c31f45d4a76f1fdcbd21
alexanderhernandez17 <alexander.hdez.2001@gmail.com>
2024-05-27 14:20:48 -0400

    first commit

---
+++ src/car/entities/car.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'cars_alexanderhernandez'})
+export class CarFavorite {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+    
+    @Column()
+    brand: string;
+
+    @Column()
+    model: string;
+
+    @Column()
+    year: number;
+
+    @Column()
+    color: string;
+
+    @Column()
+    combustion_type: string;
+    
+}
